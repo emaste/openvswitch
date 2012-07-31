860a11d6735d2fc2c339ce7b870ab2759300ea9c
Ben Pfaff <blp@nicira.com>
2012-07-31 10:12:20 -0700

    test-vconn: Block in three cases where a race is visible on FreeBSD.
    On FreeBSD, sometimes plain vconn_connect() or vconn_recv() reports EAGAIN
    in these cases.
    
    Reported-by: Ed Maste <emaste@freebsd.org>
    Signed-off-by: Ben Pfaff <blp@nicira.com>
    
    Conflicts:
    	lib/vconn.h

--- tests/test-vconn.c
+++ tests/test-vconn.c
@@ -153,7 +153,7 @@ test_refuse_connection(int argc OVS_UNUSED, char *argv[])
                            DSCP_DEFAULT), 0);
     fpv_close(&fpv);
     vconn_run(vconn);
-    CHECK_ERRNO(vconn_connect(vconn), expected_error);
+    CHECK_ERRNO(vconn_connect_block(vconn), expected_error);
     vconn_close(vconn);
     fpv_destroy(&fpv);
 }
@@ -223,7 +223,7 @@ test_read_hello(int argc OVS_UNUSED, char *argv[])
        poll_block();
     }
     stream_close(stream);
-    CHECK_ERRNO(vconn_connect(vconn), ECONNRESET);
+    CHECK_ERRNO(vconn_connect_block(vconn), ECONNRESET);
     vconn_close(vconn);
 }
 
@@ -313,7 +313,7 @@ test_send_hello(const char *type, const void *out, size_t out_size,
        poll_block();
     }
     stream_close(stream);
-    CHECK_ERRNO(vconn_recv(vconn, &msg), EOF);
+    CHECK_ERRNO(vconn_recv_block(vconn, &msg), EOF);
     vconn_close(vconn);
 }
 
