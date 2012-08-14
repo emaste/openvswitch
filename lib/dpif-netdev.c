d7984e80688763115b05c27746a2e51a5d435263
Ed Maste <emaste@freebsd.org>
2012-08-14 16:48:16 -0400

    Add missing part of unidirectional pipe fix
    commit 9018615e682fec7ea147e6220e64b3c04121ee9b switched to using two
    pipes to avoid a non-portable bidirectional pipe, but one of the two
    ends was not changed.

--- lib/dpif-netdev.c
+++ lib/dpif-netdev.c
@@ -1394,7 +1394,7 @@ dp_thread_body(void *args OVS_UNUSED)
             dp = (struct dp_netdev *)node->data;
             if (dp->pipe_fd && (dp->pipe_fd->revents & POLLIN)) {
                 VLOG_DBG("Signalled from main thread");
-                while ((error = read(dp->pipe[1], readbuf, sizeof(readbuf))) > 0)
+                while ((error = read(dp->rpipe[0], readbuf, sizeof(readbuf))) > 0)
                         ;
                 if (error < 0 && errno != EAGAIN) {
                     VLOG_ERR("Pipe read error (to datapath): %s", strerror(errno));
