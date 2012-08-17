5718283c720957b483b1e5c613213c0f2e47e7be
Ed Maste <emaste@freebsd.org>
2012-08-17 15:15:38 -0400

    Whitespace: eliminate hard tabs

--- lib/netdev-dummy.c
+++ lib/netdev-dummy.c
@@ -59,7 +59,7 @@ struct netdev_dummy {
     bool listening;
 #ifdef THREADED
     pthread_mutex_t queue_mutex;
-    int s_pipe[2];		/* used to signal packet arrivals */
+    int s_pipe[2];              /* used to signal packet arrivals */
 #endif
 };
 
@@ -158,8 +158,8 @@ netdev_dummy_close(struct netdev *netdev_)
     ofpbuf_list_delete(&netdev->recv_queue);
 #ifdef THREADED
     if (netdev->listening) {
-	close(netdev->s_pipe[0]);
-	close(netdev->s_pipe[1]);
+        close(netdev->s_pipe[0]);
+        close(netdev->s_pipe[1]);
         pthread_mutex_destroy(&netdev->queue_mutex);
     }
 #endif
